c547af8effcd20a3f165ef7ca502aa99df211c6f
EC2 Default User <ec2-user@ip-172-31-37-211.eu-north-1.compute.internal>
2025-07-03 04:02:29 +0000

    files added.

--- Python/py-app/app.py
+++ Python/py-app/app.py
@@ -12,4 +12,4 @@ def home():
     return render_template('index.html', name="Devik Jagani", projects=projects)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
